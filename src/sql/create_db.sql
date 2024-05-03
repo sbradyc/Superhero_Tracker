622930e2bf21debc88e226fbfcf1993a2145f4fa
Sean Brady <bradysea@oregonstate.edu>
2024-05-03 17:49:53 -0500

    Add intersection table for mission and cities.
    Due to how a missions can be in multiple cities, an intersection table should be made to handle the M:N nature of the interaction.

--- src/sql/create_db.sql
+++ src/sql/create_db.sql
@@ -1,6 +1,22 @@
 CREATE DATABASE IF NOT EXISTS SuperTrackerDB;
 USE SuperTrackerDB;
 
+START TRANSACTION;
+
+/* For import errors */
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
+DROP TABLE IF EXISTS Countries;
+DROP TABLE IF EXISTS Cities;
+DROP TABLE IF EXISTS Heroes;
+DROP TABLE IF EXISTS Villains;
+DROP TABLE IF EXISTS Missions;
+DROP TABLE IF EXISTS Powers;
+DROP TABLE IF EXISTS HeroPowers;
+DROP TABLE IF EXISTS VillainPowers;
+DROP TABLE IF EXISTS MissionCities;
+
 /* The next queries create the tables. */
 CREATE TABLE IF NOT EXISTS Countries (
     country_id INT NOT NULL AUTO_INCREMENT,
@@ -40,13 +56,11 @@ CREATE TABLE IF NOT EXISTS Villains (
 
 CREATE TABLE IF NOT EXISTS Missions (
     mission_id INT NOT NULL AUTO_INCREMENT,
-    city_id INT NOT NULL,
     hero_id INT NOT NULL,
     villain_id INT NOT NULL,
     mission_codename VARCHAR(50) NOT NULL,
     description TEXT(1000) NOT NULL,
     PRIMARY KEY (mission_id),
-    FOREIGN KEY (city_id) REFERENCES Cities(city_id),
     FOREIGN KEY (hero_id) REFERENCES Heroes(hero_id),
     FOREIGN KEY (villain_id) REFERENCES Villains(villain_id) ON DELETE CASCADE
     /* If the villain is deleted, delete the mission too since there is no more danger. */
@@ -65,7 +79,7 @@ CREATE TABLE IF NOT EXISTS HeroPowers (
     power_id INT NOT NULL,
     PRIMARY KEY (hero_power_id),
     FOREIGN KEY (hero_id) REFERENCES Heroes(hero_id) ON DELETE CASCADE, /* If the hero is deleted, delete the power too. */
-    FOREIGN KEY (power_id) REFERENCES Powers(power_id)
+    FOREIGN KEY (power_id) REFERENCES Powers(power_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS VillainPowers (
@@ -74,7 +88,20 @@ CREATE TABLE IF NOT EXISTS VillainPowers (
     power_id INT NOT NULL,
     PRIMARY KEY (villain_power_id),
     FOREIGN KEY (villain_id) REFERENCES Villains(villain_id) ON DELETE CASCADE, /* If the villain is deleted, delete the power too. */
-    FOREIGN KEY (power_id) REFERENCES Powers(power_id)
+    FOREIGN KEY (power_id) REFERENCES Powers(power_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS MissionCities (
+    mission_city_id INT NOT NULL AUTO_INCREMENT,
+    mission_id INT NOT NULL,
+    city_id INT NOT NULL,
+    PRIMARY KEY (mission_city_id),
+    FOREIGN KEY (city_id) REFERENCES Cities(city_id) ON DELETE CASCADE,
+    FOREIGN KEY (mission_id) REFERENCES Missions(mission_id)ON DELETE CASCADE
+    /* Handles the case of one mission in multiple cities and vice versa. */
 );
 
 SHOW TABLES;
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
