0c091c547974a240ef12dc93fcdd04bf240a2b9f
Sean Brady <bradysea@oregonstate.edu>
2024-05-03 17:59:20 -0500

    Add example data to insert into MissionCities.

--- src/sql/example_inserts.sql
+++ src/sql/example_inserts.sql
@@ -28,12 +28,12 @@ VALUES
 (3, 'Killmonger', 'Erik', 'Killmonger'),
 (1, 'Kingpin', 'Wilson', 'Fisk');  /* Along with the first row, shows that one City can have multiple Villains*/
 
-INSERT INTO Missions (city_id, hero_id, villain_id, mission_codename, description)
+INSERT INTO Missions (hero_id, villain_id, mission_codename, description)
 VALUES
-(1, 1, 1, 'Ink Pen', 'Doctor Octopus is on a rampage through the city stealing industrial electrical fuses.'),
-(2, 2, 2, 'Kulling', 'Mr. Sinister has been attacking various underground ruins and stealing precious magical artifacts to give to Apocalypse.'),
-(3, 3, 3, 'Hunting Season', 'Killmonger is preparing an attack on Wakanda by stealing military technology from SHIELD.'),
-(1, 1, 2, 'Red Wolf', 'One of Mr. Sinister''s clones has been seen breaking into numerous research labs throughout the city.'); /* Along with the first and second rows, shows that one City can have multiple Missions, one Hero can have multiple Missions, and one Villain can be involved in multiple Missions */
+(1, 1, 'Ink Pen', 'Doctor Octopus is on a rampage through the city stealing industrial electrical fuses.'),
+(2, 2, 'Kulling', 'Mr. Sinister has been attacking various underground ruins and stealing precious magical artifacts to give to Apocalypse.'),
+(3, 3, 'Hunting Season', 'Killmonger is preparing an attack on Wakanda by stealing military technology from SHIELD.'),
+(1, 2, 'Red Wolf', 'One of Mr. Sinister''s clones has been seen breaking into numerous research labs throughout the city.'); /* Along with the first and second rows, shows that one City can have multiple Missions, one Hero can have multiple Missions, and one Villain can be involved in multiple Missions */
 
 INSERT INTO Powers (name, description)
 VALUES
@@ -55,6 +55,12 @@ VALUES
 (1, 3),  /* Along with the first row, shows that one Power can be possessed by multiple Villains */
 (2, 3);  /* Along with the third row, shows that one Villain can have multiple Powers */
 
+INSERT INTO MissionCities (mission_id, city_id)
+VALUES
+(4, 1), /* Show that a mission can span multiple cities */
+(4, 2),
+(1, 1), /* Show that a city can have multipe missions in it */
+(2, 1);
 
 /* Display table contents */
 SELECT * FROM Countries;
